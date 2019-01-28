550081902719b5182e8b54f8bc5c630464620eb4
lingjing <lingjing@jd.com>
2019-01-28 11:20:32 +0800

    寻找不重复的三个数相加的和

--- src/main/java/leetcodesolve/leetcode15/ThreeSum.java
+++ src/main/java/leetcodesolve/leetcode15/ThreeSum.java
@@ -21,10 +21,34 @@ public class ThreeSum {
      */
 
     public List<List<Integer>> threeSum(int[] nums) {
+        int total = 0;
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 2; i ++) {
-
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int low = i + 1;
+            int high = nums.length - 1;
+            int sum = total - nums[i];
+            while (low < high) {
+                if (nums[low] + nums[high] == sum) {
+                    res.add(Arrays.asList(nums[i], nums[low], nums[high]));
+//                    解决 [-2,  -1, -1, 3]这种情况
+                    while (low < high && nums[low] == nums[low + i]) {
+                        low++;
+                    }
+                    while (low < high && nums[high] == nums[high - 1]) {
+                        high--;
+                    }
+                    low++;
+                    high--;
+                } else if (nums[low] + nums[high] < sum) {
+                    low++;
+                } else {
+                    high--;
+                }
+            }
         }
         return res;
     }
