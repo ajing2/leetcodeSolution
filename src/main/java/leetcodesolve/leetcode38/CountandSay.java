616d4b0e8bcacfb71d6652668d7eaabed38cf329
lingjing <lingjing@jd.com>
2019-03-13 17:51:21 +0800

    38 没做完

---
+++ src/main/java/leetcodesolve/leetcode38/CountandSay.java
@@ -0,0 +1,28 @@
+package leetcodesolve.leetcode38;
+
+/**
+ * @ClassName CountandSay
+ * @Description TODO
+ * @Author lingxiangxiang
+ * @Date 10:04 AM
+ * @Version 1.0
+ **/
+public class CountandSay {
+    /**
+     1.     1
+     2.     11
+     3.     21
+     4.     1211
+     5.     111221
+
+     time : 不知道
+     space : O(n)
+
+     * @param n
+     * @return
+     */
+
+    public String countAndSay(int n) {
+        return null;
+    }
+}
